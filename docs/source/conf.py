94be7910cd741c31a58574f7cc77ddb20696012c
Dave Dittrich <dave.dittrich@gmail.com>
2018-01-11 09:54:55 -0800

    Switch logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -138,7 +138,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'UW-logo.png'
+html_logo = 'd2-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
