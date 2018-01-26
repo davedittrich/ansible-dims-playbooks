339492e4f40af7cd206ac8f34d12365c7e56f5a6
Dave Dittrich <dave.dittrich@gmail.com>
2018-01-25 19:49:41 -0800

    Bump version: 3.0.5 → 3.0.6

--- files/common-scripts/dims.function.sh
+++ files/common-scripts/dims.function.sh
@@ -39,7 +39,7 @@ else
 fi
 
 # Tracks with bumpversion
-DIMS_VERSION=3.0.5
+DIMS_VERSION=3.0.6
 
 # Define command line options
 DEFINE_boolean 'debug' false 'enable debug mode' 'd'
--- roles/git-ssh/files/git-shell-commands/list.sh
+++ roles/git-ssh/files/git-shell-commands/list.sh
@@ -36,7 +36,7 @@
 . $DIMS/bin/dims_functions.sh
 
 # Tracks with bumpversion
-VERSION=3.0.5
+VERSION=3.0.6
 
 export FQDN=$(get_fqdn)
 DEPLOYMENT=${DEPLOYMENT:-$(get_deployment_from_fqdn)}
