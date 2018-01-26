e9ecfe163e1612c340061474d9faf61c7c79850d
Dave Dittrich <dave.dittrich@gmail.com>
2018-01-25 19:48:40 -0800

    Switch to d2 logo in docs and cookiecutter

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -196,7 +196,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at
 # the top of the title page.
-latex_logo = 'UW-logo.png'
+latex_logo = 'd2-logo.png'
 
 # For "manual" documents, if this is true, then toplevel headings
 # are parts, not chapters.
