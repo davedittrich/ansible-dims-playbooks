d7bf769742349992050a382912d35ba7be0ad60a
Dave Dittrich <dave.dittrich@gmail.com>
2018-02-08 22:43:28 -0800

    Bump version: 3.2.0 → 3.2.1

--- roles/git-ssh/files/git-shell-commands/mrconfig.sh
+++ roles/git-ssh/files/git-shell-commands/mrconfig.sh
@@ -38,7 +38,7 @@
 # specified on the command line, or all bare repos in $GITDIR.
 
 # Tracks with bumpversion
-VERSION=3.2.0
+VERSION=3.2.1
 
 export FQDN=$(get_fqdn)
 DEPLOYMENT=${DEPLOYMENT:-$(get_deployment_from_fqdn)}
