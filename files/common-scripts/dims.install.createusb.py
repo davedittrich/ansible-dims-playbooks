87f7164b409d1370c5c98cb527601427904376f6
Dave Dittrich <dave.dittrich@gmail.com>
2018-03-24 13:04:39 -0700

    Bump version: 3.2.5 → 3.3.0

--- files/common-scripts/dims.install.createusb.py
+++ files/common-scripts/dims.install.createusb.py
@@ -71,7 +71,7 @@ from sh import udisksctl
 from sh import Command
 
 # Managed by bumpversion
-VERSION = "3.2.5"
+VERSION = "3.3.0"
 
 # Define a globals class
 class mem(object):
--- files/common-scripts/dims.updatecheck.sh
+++ files/common-scripts/dims.updatecheck.sh
@@ -33,7 +33,7 @@
 . $DIMS/bin/dims_functions.sh
 
 # Tracks with bumpversion
-DIMS_VERSION=3.2.5
+DIMS_VERSION=3.3.0
 
 FLAGS_HELP="usage: $BASE [options]"
 
