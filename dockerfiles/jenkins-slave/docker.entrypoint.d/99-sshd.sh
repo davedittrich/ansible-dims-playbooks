e9fa14c5fd462ff6253370e539e124d702bcf8be
Dave Dittrich <dave.dittrich@gmail.com>
2018-04-08 16:38:10 -0700

    Add initial jenkins-slave Docker directory

---
+++ dockerfiles/jenkins-slave/docker.entrypoint.d/99-sshd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2018, Dave Dittrich <dave.dittrich@gmail.com>. All rights reserved.
+
+
+# IMAGE, VERSION and AUTHOR are set in Dockerfile at build time.
+IMAGE="$(cat /IMAGE)"
+VERSION="$(cat /VERSION)"
+AUTHOR="$(cat /AUTHOR)"
+LAST_UPDATED="$(cat /LAST_UPDATED)"
+
+# This script is here to serve as an example and demonstration of how an
+# entrypoint modular script works and testing the "dims/base" image.
+# It can't be deleted from the image, but the script will only run
+# if the image name matches the name of the base image.
+
+/usr/sbin/sshd -D
+exit $?
